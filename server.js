0382a6f80c0eebfeb19aa14902c3fa5e2d7b3f04
Harvey Zhao <86583647+harveyzzhao@users.noreply.github.com>
2023-07-22 16:34:16 -0700

    using only client side to render bpm

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-const WebSocket = require('ws');
-const mqtt = require('mqtt')
+// const WebSocket = require('ws');
+// const mqtt = require('mqtt')
 // const url = require('url')
 
-// Setup websocket server
-var ws_host = "localhost";
-var ws_port = "8080";
-const wss = new WebSocket.Server({ host: ws_host, port: ws_port });
-var ws = null;
+// // Setup websocket server
+// var ws_host = "localhost";
+// var ws_port = "8080";
+// const wss = new WebSocket.Server({ host: ws_host, port: ws_port });
+// var ws = null;
  
-// Setup MQTT Client
-// mqtt[s]://[username][:password]@host.domain[:port]
-var url = 'mqtt://test.mosquitto.org:1883'
-// var url = 'mqtt://172.20.10.2:9090'
+// // Setup MQTT Client
+// // mqtt[s]://[username][:password]@host.domain[:port]
+// var url = 'mqtt://test.mosquitto.org:1883'
+// // var url = 'mqtt://172.20.10.2:9090'
  
-const options = {
-  // Clean session
-  port: 1883,
-  clean: true,
-  connectTimeout: 4000,
-}
+// const options = {
+//   // Clean session
+//   port: 1883,
+//   clean: true,
+//   connectTimeout: 4000,
+// }
 
-const client  = mqtt.connect(url, options)
+// const client  = mqtt.connect(url, options)
 
-client.on('connect', function () {
-  console.log('Connected')
-  client.subscribe('ucla/hack', function (err) {
-    if (!err) {
-      client.publish('ucla/hack', 'woof')
-    }
-  })
-})
+// client.on('connect', function () {
+//   console.log('Connected')
+//   client.subscribe('ucla/hack', function (err) {
+//     if (!err) {
+//       client.publish('ucla/hack', 'woof')
+//     }
+//   })
+// })
 
-// Receive messages
-client.on('message', function (topic, message) {
-  console.log(message.toString())
-  wss.clients.forEach(function each(ws) {
-    if (ws.isAlive === false) return ws.terminate();
-    ws.send(message.toString());
-  });
-})
+// // Receive messages
+// client.on('message', function (topic, message) {
+//   console.log(message.toString())
+//   wss.clients.forEach(function each(ws) {
+//     if (ws.isAlive === false) return ws.terminate();
+//     ws.send(message.toString());
+//   });
+// })
--- src/main.js
+++ src/main.js
@@ -1,9 +1,77 @@
-import './style.css'
-import './client.js'
-import heartLogo from './assets/heart.svg'
+import "./style.css";
+import heartLogo from "./assets/heart.svg";
 
+// set up Paho MQTT client
+let env = {
+  broker: "broker.hivemq.com",
+  port: 8000,
+  clientID: "web" + new Date().getTime(),
+};
 
-document.querySelector('#app').innerHTML = `
+const client = new Paho.Client(env.broker, env.port, env.clientID);
+
+const sendMessage = (bpm) => {
+  // Replace this function with your actual function to send the message
+  console.log("Sending message:", bpm);
+  const message = new Paho.Message(bpm); // message the heart rate to the broker
+  message.destinationName = "ucla/hack";
+  client.send(message);
+};
+
+const sendMessagesInIntervals = () => {
+  const messages = [
+    "Message 1",
+    "Message 2",
+    "Message 3",
+    "Message 4",
+    "Message 5",
+  ];
+  let index = 0;
+
+  const sendNextMessage = () => {
+    if (index < messages.length) {
+      const message = messages[index];
+      sendMessage(message);
+      index++;
+
+      // Set a 1 millisecond delay and call sendNextMessage again
+      setTimeout(sendNextMessage, 1000);
+    }
+  };
+
+  // Start sending messages
+  sendNextMessage();
+};
+
+const onConnect = () => {
+  console.log("Connected");
+  client.subscribe("ucla/hack");
+  const message = new Paho.Message("100"); // message the heart rate to the broker
+  message.destinationName = "ucla/hack";
+  client.send(message);
+
+  // Call the function to start sending messages in intervals
+  sendMessagesInIntervals();
+};
+
+const onConnectionLost = (responseObject) => {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:" + responseObject.errorMessage);
+  }
+};
+
+const onMessageArrived = (message) => {
+  console.log("Message received");
+  console.log(message.payloadString);
+  const element = document.querySelector("#bpm");
+  element.innerHTML = message.payloadString;
+};
+
+client.connect({ onSuccess: onConnect });
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://etransfercenter.seas.ucla.edu/" target="_blank">
       <img src="${heartLogo}" class="logo heart" alt="Heart" />
@@ -12,4 +80,4 @@ document.querySelector('#app').innerHTML = `
       <button id="bpm" type="button"></button>
     </div>
   </div>
-`
+`;
