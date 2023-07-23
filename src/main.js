af1ca496a81c9cd771678c954f7c48d99aef5f50
Harvey Zhao <86583647+harveyzzhao@users.noreply.github.com>
2023-07-22 20:09:50 -0700

    add animation to heart

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import "./style.css";
 import heartLogo from "./assets/heart.svg";
 
+document.querySelector("#app").innerHTML = `
+  <div>
+    <a href="https://etransfercenter.seas.ucla.edu/" target="_blank">
+      <img src="${heartLogo}" class="heart" alt="Heart" />
+    </a>
+    <div class="card">
+      <button id="bpm" type="button">Loading...</button>
+    </div>
+  </div>
+`;
+
 // set up Paho MQTT client
 let env = {
   broker: "broker.hivemq.com",
@@ -19,23 +30,16 @@ const sendMessage = (bpm) => {
 };
 
 const sendMessagesInIntervals = () => {
-  const messages = [
-    "Message 1",
-    "Message 2",
-    "Message 3",
-    "Message 4",
-    "Message 5",
-  ];
+  const messages = ["1", "2", "3", "4", "5"];
   let index = 0;
 
   const sendNextMessage = () => {
-    if (index < messages.length) {
-      const message = messages[index];
+    if (true) {
+      const message = messages[index % messages.length];
       sendMessage(message);
       index++;
-
       // Set a 1 millisecond delay and call sendNextMessage again
-      setTimeout(sendNextMessage, 1000);
+      setTimeout(sendNextMessage, 800);
     }
   };
 
@@ -61,23 +65,35 @@ const onConnectionLost = (responseObject) => {
 };
 
 const onMessageArrived = (message) => {
-  console.log("Message received");
-  console.log(message.payloadString);
-  const element = document.querySelector("#bpm");
-  element.innerHTML = message.payloadString;
+  console.log("Message received " + message.payloadString);
+  const bpmDisplay = document.querySelector("#bpm");
+  bpmDisplay.innerHTML = message.payloadString;
+
+  const animateHeartBeat = () => {
+    // make heart beat for every received message
+    console.log("set drop shadow");
+    const heart = document.querySelector(".heart");
+    heart.style.filter = "drop-shadow(0 0 1em rgba(255, 0, 0, 1))";
+
+    setTimeout(() => {
+      const fadeDropShadow = () => {
+        console.log("fadeDropShadow");
+        let opacity = 1;
+        const interval = setInterval(function () {
+          opacity -= 0.2;
+          // Update the filter style with the new opacity
+          heart.style.filter = `drop-shadow(0 0 1em rgba(255, 0, 0, ${opacity}))`;
+          if (opacity <= 0) {
+            clearInterval(interval); // Stop the interval when opacity reaches 0
+          }
+        }, 40); // Interval of 100 milliseconds (adjust as needed for smoothness)
+      };
+      fadeDropShadow();
+    }, 1);
+  };
+  animateHeartBeat();
 };
 
 client.connect({ onSuccess: onConnect });
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
-
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://etransfercenter.seas.ucla.edu/" target="_blank">
-      <img src="${heartLogo}" class="logo heart" alt="Heart" />
-    </a>
-    <div class="card">
-      <button id="bpm" type="button"></button>
-    </div>
-  </div>
-`;
